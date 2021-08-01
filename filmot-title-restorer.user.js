bb7ce0aaa40cd40ae83bf8002b443e35da776ef0
Jopik1 <47256803+Jopik1@users.noreply.github.com>
2021-08-01 18:17:28 +0300

    only show button if deleted videos are present, changed callback injection method
    only show button if deleted videos are present, changed callback injection method

--- filmot-title-restorer.user.js
+++ filmot-title-restorer.user.js
@@ -2,31 +2,31 @@
 // @name         Filmot Title Restorer
 // @namespace    http://tampermonkey.net/
 // @version      0.1
+// @license GPLv3
 // @description  Restores titles for removed or private videos in YouTube playlists
 // @author       Jopik
 // @match        https://*.youtube.com/watch*
 // @icon         https://www.google.com/s2/favicons?domain=filmot.com
-// @grant        none
+// @grant        unsafeWindow
 // @require      http://code.jquery.com/jquery-3.4.1.min.js
-// @license      GPLv3
 // ==/UserScript==
 
 
 /*--- Create a button in a container div.  It will be styled and
     positioned with CSS.
 */
-var r= $('<button id="TitleRestoredBtn">Restore Titles</button>');
-$("#secondary.ytd-watch-flexy").first().prepend(r);
 
-//--- Activate the newly added button.
-document.getElementById ("TitleRestoredBtn").addEventListener (
-    "click", ButtonClickAction, false
-);
-
-
+$(document).ready(function() { //When document has loaded
+    setTimeout(function() {
+        extractIDs();
+    }, 2000);
+});
 
 function extractIDs() {
-	var deleted_ids="";
+    window.deletedIDs="";
+    window.deletedIDCnt=0;
+	var deletedIDs="";
+    var deletedIDsCnt=0;
 	var a=$("a.ytd-playlist-panel-video-renderer").filter(function() {
 		return $(this).find("#video-title.ytd-playlist-panel-video-renderer[title='']").length>0;
 	}).each(function( index, element ) {
@@ -34,32 +34,44 @@ function extractIDs() {
 		var href=$(element).attr('href');
 		var id=String(href.match(/v=[0-9A-Za-z_\-]*/gm));
 		id=id.substring(2);
-		if (deleted_ids.length>0)
+		if (deletedIDs.length>0)
         {
-            deleted_ids+=",";
+            deletedIDs+=",";
         }
-		deleted_ids+=id;
+		deletedIDs+=id;
+        deletedIDsCnt++;
 	});
 
+    if (deletedIDs.length>0) {
+        window.deletedIDs=deletedIDs;
+        window.deletedIDCnt=deletedIDsCnt;
+        var r= $('<button id="TitleRestoredBtn">Restore Titles</button>');
+        $("#secondary.ytd-watch-flexy").first().prepend(r);
 
-    var code=`function fetchDoneIDs() {
-        for (let i = 0; i < window.fetched_details.length; ++i) {
-            sel="a.ytd-playlist-panel-video-renderer[href*='"+ window.fetched_details[i].id+"']";
+        //--- Activate the newly added button.
+        document.getElementById ("TitleRestoredBtn").addEventListener (
+            "click", ButtonClickAction, false
+        );
+
+
+    }
+}
+
+function processClick() {
+    unsafeWindow.fetchDoneIDs=function (fetched_details) {
+        for (let i = 0; i < fetched_details.length; ++i) {
+            var sel="a.ytd-playlist-panel-video-renderer[href*='"+ fetched_details[i].id+"']";
             $(sel).css("background-color","#6ab8f7");
-            $(sel).find("#video-title").text(window.fetched_details[i].title);
-            $(sel).find("#byline").text(window.fetched_details[i].channelname);
+            $(sel).find("#video-title").text(fetched_details[i].title);
+            $(sel).find("#byline").text(fetched_details[i].channelname);
         }
-    }`;
-
-    var JS= document.createElement('script');
-    JS.text= code;
-    document.body.appendChild(JS);
-
-    document.body.appendChild(document.createElement('script')).src='https://filmot.com/api/getvideos?callback=fetchDoneIDs&js=1&key=md5paNgdbaeudounjp39&id='+ deleted_ids;
-
+        $("#TitleRestoredBtn").text(fetched_details.length+ " of " + window.deletedIDCnt + " restored");
+    };
+  
+    document.body.appendChild(document.createElement('script')).src='https://filmot.com/api/getvideos?callback=window.fetchDoneIDs&js=1&key=md5paNgdbaeudounjp39&id='+ window.deletedIDs;
 }
 
 function ButtonClickAction (zEvent) {
-    extractIDs();
+    processClick();
     return false;
 }
