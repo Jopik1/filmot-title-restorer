71e35b2eff02b5ad97fa2bb20ef300fb162ddb59
Jopik1 <47256803+Jopik1@users.noreply.github.com>
2021-08-01 18:22:56 +0300

    change license short code

--- filmot-title-restorer.user.js
+++ filmot-title-restorer.user.js
@@ -2,7 +2,7 @@
 // @name         Filmot Title Restorer
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @license      GPL-3.0
+// @license      GPL-3.0-only
 // @description  Restores titles for removed or private videos in YouTube playlists
 // @author       Jopik
 // @match        https://*.youtube.com/watch*
