9a835618710f29b99785cb23c36a187522b15916
Jopik1 <47256803+Jopik1@users.noreply.github.com>
2021-08-03 22:47:04 +0300

    changed highlight color
    changed highlight color to make channel name easier to read

--- filmot-title-restorer.user.js
+++ filmot-title-restorer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Filmot Title Restorer
 // @namespace    http://tampermonkey.net/
-// @version      0.21
+// @version      0.23
 // @license GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
 // @description  Restores titles for removed or private videos in YouTube playlists
 // @author       Jopik
@@ -13,7 +13,7 @@
 
 document.addEventListener('yt-navigate-start', handleNavigateStart);
 document.addEventListener('yt-navigate-finish', handleNavigateFinish);
-GM_addStyle("filmot_hide { display: none;} filmot_highlight {background-color: #6ab8f7;} ")
+GM_addStyle("filmot_hide { display: none;} filmot_highlight {background-color: #b0f2f4;} ")
 
 function escapeHTML(unsafe) {
   return unsafe.replace(
@@ -78,7 +78,7 @@ function processClick() {
         for (let i = 0; i < fetched_details.length; ++i) {
             var sel="a.ytd-playlist-panel-video-renderer[href*='"+ fetched_details[i].id+"']";
             var item=$(sel);
-            item.css("background-color","#6ab8f7");
+            item.css("background-color","#b0f2f4");
             item.addClass("filmot_highlight");
             item.find("#video-title").text(fetched_details[i].title);
             item.find("#byline").text(fetched_details[i].channelname);
