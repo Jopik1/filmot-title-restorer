eb9b431625fbffe3c81ae4c7bc621f885d1535ad
Jopik1 <47256803+Jopik1@users.noreply.github.com>
2021-08-04 22:15:22 +0300

    added support for full format playlists
    Added support for full format playlists https://www.youtube.com/playlist?list=PLU1qYmzYerlrMNslZ8C7Q3f9qgPha9Diy
    Click on the ... button in the playlist menu and select "Show unavailable videos"

--- filmot-title-restorer.user.js
+++ filmot-title-restorer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Filmot Title Restorer
 // @namespace    http://tampermonkey.net/
-// @version      0.24
+// @version      0.3
 // @license GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
 // @description  Restores titles for removed or private videos in YouTube playlists
 // @author       Jopik
@@ -13,6 +13,8 @@
 
 document.addEventListener('yt-navigate-start', handleNavigateStart);
 document.addEventListener('yt-navigate-finish', handleNavigateFinish);
+document.addEventListener( 'yt-action', handlePageDataLoad );
+
 GM_addStyle("filmot_hide { display: none;} filmot_highlight {background-color: #b0f2f4;} ");
 
 //fire at least once on load, sometimes handleNavigateFinish on first load yt-navigate-finish already fired before script loads
@@ -25,6 +27,17 @@ function escapeHTML(unsafe) {
   )
 }
 
+function handlePageDataLoad(event){
+    if (event.detail!=null && event.detail.actionName!=null && event.detail.actionName.indexOf("yt-append-continuation")>=0) {
+        console.log("filmot yt-append-continuation");
+        console.log(event);
+        if (window.location.href.indexOf("/playlist?")>0)
+        {
+            extractIDsFullView();
+        }
+    }
+}
+
 function handleNavigateStart(){
     var filmotTitles=$(".filmot_title");
     filmotTitles.text("");
@@ -34,13 +47,20 @@ function handleNavigateStart(){
     filmotChannels.attr("onclick","");
     filmotChannels.removeClass("filmot_channel");
     cleanUP();
-    //console.log("filmot handleNavigateStart");
+    console.log("filmot handleNavigateStart");
 }
 
 function handleNavigateFinish(){
     cleanUP();
-    //console.log("filmot handleNavigateFinish");
-    setTimeout(function(){ extractIDs(); }, 500);
+    console.log("filmot handleNavigateFinish");
+    if (window.location.href.indexOf("/playlist?")>0)
+    {
+        setTimeout(function(){ extractIDsFullView(); }, 500);
+    }
+    else
+    {
+        setTimeout(function(){ extractIDsSideView(); }, 500);
+    }
 }
 
 function cleanUP() {
@@ -50,21 +70,26 @@ function cleanUP() {
     $(".filmot_highlight").css("background-color","");
     $(".filmot_highlight").removeClass("filmot_highlight");
     $("#TitleRestoredDiv").remove();
+    $(".filmot_c_link").remove();
+    window.RecoveredIDS={};
+    window.DetectedIDS={};
 }
 
-function extractIDs() {
+function extractIDsFullView() {
     window.deletedIDs="";
     window.deletedIDCnt=0;
 	var deletedIDs="";
     var deletedIDsCnt=0;
-    //console.log("filmot extractIDs");
-	var a=$("a.ytd-playlist-panel-video-renderer").filter(function() {
-		return $(this).find("#video-title.ytd-playlist-panel-video-renderer[title='']").length>0;
+    console.log("filmot extractIDs");
+    var rendererSelector="a.ytd-playlist-video-renderer";
+	var a=$(rendererSelector).filter(function() {
+        return !$(this).attr('aria-label');
 	}).each(function( index, element ) {
 		// element == this
 		var href=$(element).attr('href');
 		var id=String(href.match(/v=[0-9A-Za-z_\-]*/gm));
 		id=id.substring(2);
+        window.DetectedIDS[id]=1;
 		if (deletedIDs.length>0)
         {
             deletedIDs+=",";
@@ -73,14 +98,58 @@ function extractIDs() {
         deletedIDsCnt++;
 	});
 
+    console.log("IDS: " + deletedIDs);
     if (deletedIDs.length>0) {
         window.deletedIDs=deletedIDs;
         window.deletedIDCnt=deletedIDsCnt;
-        var r= $('<div id="TitleRestoredDiv"><center><button id="TitleRestoredBtn">Restore Titles</button><br><a href="https://filmot.com" target="_blank">Powered by filmot.com</a></center></div>');
-        $("#container.ytd-playlist-panel-renderer").first().prepend(r);
-        document.getElementById ("TitleRestoredBtn").addEventListener (
-                "click", ButtonClickAction, false
+        if (document.getElementById ("TitleRestoredBtn")==null)
+        {
+            var r= $('<div id="TitleRestoredDiv"><center><button id="TitleRestoredBtn">Restore Titles</button><br><a href="https://filmot.com" target="_blank">Powered by filmot.com</a></center></div>');
+            $("#items.ytd-playlist-sidebar-renderer").first().prepend(r);
+            document.getElementById ("TitleRestoredBtn").addEventListener (
+                "click", ButtonClickActionFullView, false
             );
+        }
+    }
+    processClick(2);
+}
+
+
+function extractIDsSideView() {
+    window.deletedIDs="";
+    window.deletedIDCnt=0;
+	var deletedIDs="";
+    var deletedIDsCnt=0;
+    console.log("filmot extractIDs");
+    var rendererSelector="a.ytd-playlist-panel-video-renderer";
+	var a=$(rendererSelector).filter(function() {
+		return $(this).find("#video-title.ytd-playlist-panel-video-renderer[title='']").length>0;
+	}).each(function( index, element ) {
+		// element == this
+		var href=$(element).attr('href');
+		var id=String(href.match(/v=[0-9A-Za-z_\-]*/gm));
+		id=id.substring(2);
+        window.DetectedIDS[id]=1;
+		if (deletedIDs.length>0)
+        {
+            deletedIDs+=",";
+        }
+		deletedIDs+=id;
+        deletedIDsCnt++;
+	});
+
+    //console.log(deletedIDs);
+    if (deletedIDs.length>0) {
+        window.deletedIDs=deletedIDs;
+        window.deletedIDCnt=deletedIDsCnt;
+        if (document.getElementById ("TitleRestoredBtn")==null)
+        {
+            var r= $('<div id="TitleRestoredDiv"><center><button id="TitleRestoredBtn">Restore Titles</button><br><a href="https://filmot.com" target="_blank">Powered by filmot.com</a></center></div>');
+            $("#container.ytd-playlist-panel-renderer").first().prepend(r);
+            document.getElementById ("TitleRestoredBtn").addEventListener (
+                "click", ButtonClickActionSideView, false
+            );
+        }
     }
 }
 
@@ -89,41 +158,105 @@ function reportAJAXError(error)
     alert("Error fetching API results " + error);
 }
 
-function processJSONResult (fetched_details)
+function processJSONResultSideView (fetched_details,format)
 {
     for (let i = 0; i < fetched_details.length; ++i) {
-        var escapedTitle=escapeHTML(fetched_details[i].title);
-        var sel="a.ytd-playlist-panel-video-renderer[href*='"+ fetched_details[i].id+"']";
+        var meta=fetched_details[i];
+        window.RecoveredIDS[meta.id]=1;
+        var escapedTitle=escapeHTML(meta.title);
+        if (meta.channelname==null) {
+            meta.channelname=fetched_details[i].channelid;
+        }
+
+
+        var sel="a.ytd-playlist-panel-video-renderer[href*='"+ meta.id+"']";
         var item=$(sel);
+
+        //console.log(item);
 
         item.css("background-color","#b0f2f4");
         item.addClass("filmot_highlight");
 
         var titleItem=item.find("#video-title");
-        titleItem.text(fetched_details[i].title);
-        titleItem.attr("title",fetched_details[i].title);
-        titleItem.attr("aria-label",fetched_details[i].title);
+        titleItem.text(meta.title);
+        titleItem.attr("title",meta.title);
+        titleItem.attr("aria-label",meta.title);
         titleItem.addClass("filmot_title");
 
         var channelItem=item.find("#byline");
+
         channelItem.text(fetched_details[i].channelname);
         channelItem.attr("onclick","window.open('https://www.youtube.com/channel/" +fetched_details[i].channelid + "', '_blank'); event.stopPropagation(); return false;");
         channelItem.addClass("filmot_channel");
 
         item.find(".filmot_newimg").remove();
-        var newThumb='<img id="filmot_newimg" class="style-scope yt-img-shadow filmot_newimg" onclick="prompt(\'Full Title\', \''+ escapedTitle+ '\'); event.stopPropagation(); return false;" title="' + escapedTitle + '" width="100" src="https://filmot.com/vi/' + fetched_details[i].id + '/default.jpg">';
+        var newThumb='<img id="filmot_newimg" class="style-scope yt-img-shadow filmot_newimg" onclick="prompt(\'Full Title\', \''+ escapedTitle+ '\'); event.stopPropagation(); return false;" title="' + escapedTitle + '" width="100" src="https://filmot.com/vi/' + meta.id + '/default.jpg">';
         item.find("yt-img-shadow.ytd-thumbnail").append(newThumb);
         item.find("#img.yt-img-shadow").addClass("filmot_hide");
         item.find("#img.yt-img-shadow").hide();
     }
-    $("#TitleRestoredBtn").text(fetched_details.length+ " of " + window.deletedIDCnt + " restored");
+
+    $("#TitleRestoredBtn").text(Object.keys(window.RecoveredIDS).length+ " of " +Object.keys(window.DetectedIDS).length + " restored");
 }
 
-function processClick()
+
+function processJSONResultFullView (fetched_details,format)
+{
+    for (let i = 0; i < fetched_details.length; ++i) {
+        var meta=fetched_details[i];
+        window.RecoveredIDS[meta.id]=1;
+        var escapedTitle=escapeHTML(meta.title);
+
+        if (meta.channelname==null) {
+            meta.channelname=fetched_details[i].channelid;
+        }
+
+        var rendererSelector="#container.ytd-playlist-video-renderer";
+        var a=$(rendererSelector).filter(function() {
+            return $(this).find("a.ytd-playlist-video-renderer[href*='"+ meta.id+"']").length>0;
+        }).each(function( index, element ) {
+            // element == this
+            var item=$(element);
+            item.css("background-color","#b0f2f4");
+            item.addClass("filmot_highlight");
+
+            var titleItem=item.find("#video-title");
+            titleItem.text(meta.title);
+            titleItem.attr("title",meta.title);
+            titleItem.attr("aria-label",meta.title);
+            titleItem.addClass("filmot_title");
+
+            var channelItem=item.find("yt-formatted-string.ytd-channel-name");
+            //console.log(channelItem);
+            channelItem.find("a.filmot_c_link").remove();
+            var channelLink="<a class='filmot_c_link' href='https://www.youtube.com/channel/" +meta.channelid + "'>" + escapeHTML(meta.channelname) + "</a>";
+            channelItem.append(channelLink);
+
+            item.find("#byline-container").attr("hidden",false);
+            item.find(".filmot_newimg").remove();
+            var newThumb='<img id="filmot_newimg" class="style-scope yt-img-shadow filmot_newimg" onclick="prompt(\'Full Title\', \''+ escapedTitle+ '\'); event.stopPropagation(); return false;" title="' + escapedTitle + '" width="100" src="https://filmot.com/vi/' + meta.id + '/default.jpg">';
+            item.find("yt-img-shadow.ytd-thumbnail").append(newThumb);
+            item.find("#img.yt-img-shadow").addClass("filmot_hide");
+            item.find("#img.yt-img-shadow").hide();
+
+        });
+
+    }
+
+    $("#TitleRestoredBtn").text(Object.keys(window.RecoveredIDS).length+ " of " + Object.keys(window.DetectedIDS).length + " restored");
+}
+
+function processClick(format)
 {
     var apiURL='https://filmot.com/api/getvideos?key=md5paNgdbaeudounjp39&id='+ window.deletedIDs;
     var jqxhr = $.getJSON(apiURL, function(data) {
-        processJSONResult(data);
+        if (format==1) {
+            processJSONResultSideView(data,format);
+        }
+        else
+        {
+            processJSONResultFullView(data,format);
+        }
     })
     .done(function(data) {
     })
@@ -135,7 +268,13 @@ function processClick()
 
 }
 
-function ButtonClickAction (zEvent) {
-    processClick();
+function ButtonClickActionSideView (zEvent) {
+    processClick(1);
     return false;
 }
+
+function ButtonClickActionFullView (zEvent) {
+    processClick(2);
+    return false;
+}
+
