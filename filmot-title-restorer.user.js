575425bfd3ef6101a0f17dbea929b42182b3b306
Samg381 <samg381@yahoo.com>
2025-06-06 01:07:48 -0400

    Versioning

--- filmot-title-restorer.user.js
+++ filmot-title-restorer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Filmot Title Restorer
 // @namespace    http://tampermonkey.net/
-// @version      0.42
+// @version      0.43
 // @license GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
 // @description  Restores titles for removed or private videos in YouTube playlists
 // @author       Jopik
